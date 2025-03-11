113d7461a7bcc1a76962c35cc1e994920d206019
Austin Owensby <austin_owensby@hotmail.com>
2025-03-11 07:22:12 -0500

    Day 5 solution

--- Shared/Services/Solution05Service.cs
+++ Shared/Services/Solution05Service.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace I18NPuzzles.Services
 {
     // (ctrl/command + click) the link to open the input file
@@ -10,9 +12,35 @@ namespace I18NPuzzles.Services
 
             int answer = 0;
 
+            int col = 0;
+
+            // Find the true line length considering emojis
+            int lineLength = 0;
+            TextElementEnumerator iterator = StringInfo.GetTextElementEnumerator(lines[0]);
+            while (iterator.MoveNext()) {
+                lineLength++;
+            }
+
             foreach (string line in lines)
             {
+                // Iterate over text elements instead of just characters to account for emojis
+                iterator = StringInfo.GetTextElementEnumerator(line);
 
+                int index = 0;
+
+                while (iterator.MoveNext()) {
+                    if (index == col && iterator.GetTextElement() == "💩") {
+                        answer++;
+                    }
+                    index++;
+
+                    if (index > col) {
+                        break;
+                    }
+                }
+                
+                // Wrap around left to right
+                col = (col + 2) % lineLength;
             }
 
             return answer.ToString();
