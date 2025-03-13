f99d33f5b2a7591e665d96c1a070c12256ef5adb
Austin Owensby <austin_owensby@hotmail.com>
2025-03-13 07:47:57 -0500

    Day 7 solution

--- Shared/Services/Solution07Service.cs
+++ Shared/Services/Solution07Service.cs
@@ -10,9 +10,32 @@ namespace I18NPuzzles.Services
 
             int answer = 0;
 
-            foreach (string line in lines)
-            {
+            TimeZoneInfo halifaxTimeZone = TimeZoneInfo.FindSystemTimeZoneById("America/Halifax");
+            TimeZoneInfo santiagoTimeZone = TimeZoneInfo.FindSystemTimeZoneById("America/Santiago");
 
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string line = lines[i];
+
+                // Parse the data
+                List<string> data = line.QuickRegex("(.+)\\s+(\\d+)\\s+(\\d+)");
+                DateTimeOffset date = DateTimeOffset.Parse(data[0]);
+                int correctMinutes = int.Parse(data[1]);
+                int incorrectMinutes = int.Parse(data[2]);
+
+                // Determine the correct timezone by comparing the offset in minutes
+                TimeSpan halifaxUTCOffset = halifaxTimeZone.GetUtcOffset(date);
+                TimeZoneInfo correctTimeZone = halifaxUTCOffset.TotalMinutes == date.TotalOffsetMinutes ? halifaxTimeZone : santiagoTimeZone;
+
+                // Adjust the time based on the audit logs
+                DateTimeOffset utcDate = date.UtcDateTime;
+                utcDate = utcDate.AddMinutes(-incorrectMinutes);
+                utcDate = utcDate.AddMinutes(correctMinutes);
+
+                // Convert the time into the correct timezone
+                DateTimeOffset correctedDate = TimeZoneInfo.ConvertTime(utcDate, correctTimeZone);
+
+                answer += correctedDate.Hour * (i + 1);
             }
 
             return answer.ToString();
