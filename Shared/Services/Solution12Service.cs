75788704530179376ac6b3308ef7bc0587f9ef4d
Austin Owensby <austin_owensby@hotmail.com>
2025-03-18 08:20:39 -0500

    Day 12 solution

--- Shared/Services/Solution12Service.cs
+++ Shared/Services/Solution12Service.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace I18NPuzzles.Services
 {
     // (ctrl/command + click) the link to open the input file
@@ -7,13 +9,22 @@ namespace I18NPuzzles.Services
         public string RunSolution(bool example)
         {
             List<string> lines = FileUtility.GetInputLines(12, example);
+            List<(string name, int number)> phonebook = lines.Select(l => l.Split(": ")).Select(l => (l[0], int.Parse(l[1]))).ToList();
+            int midpoint = (phonebook.Count - 1) / 2;
 
-            int answer = 0;
+            List<(string name, int number)> englishPhonebook = phonebook.Select(line => (new string(line.name.Replace("æ", "ae").Replace("Æ", "AE").Normalize(NormalizationForm.FormD).Where(c => char.IsLetter(c) || c == ',').ToArray()), line.number)).ToList();
+            englishPhonebook = englishPhonebook.OrderBy(l => l.name).Select(p => (phonebook.First(pb => pb.number == p.number).name, p.number)).ToList();
+            long englishMidpoint = englishPhonebook[midpoint].number;
 
-            foreach (string line in lines)
-            {
+            List<(string name, int number)> swedishPhonebook = phonebook.Select(line => (new string(line.name.Replace("æ", "ä").Replace("Æ", "Ä").Replace("Ø", "Ö").Replace("ø", "ö").Select(c => char.IsAscii(c) || c == 'ä' || c == 'Ä' || c == 'Ö' || c == 'ö' || c == 'Å' || c == 'å' ? c : $"{c}".Normalize(NormalizationForm.FormD)[0]).Where(c => char.IsLetter(c) || c == ',').ToArray()), line.number)).ToList();
+            swedishPhonebook = swedishPhonebook.Select(p => (p.name.Replace("å", "zza").Replace("Å", "ZZa").Replace("ä", "zzb").Replace("Ä", "ZZB").Replace("ö", "zzc").Replace("Ö", "ZZC"), p.number)).OrderBy(l => l.Item1).Select(p => (phonebook.First(pb => pb.number == p.number).name, p.number)).ToList();
+            long swedishMidpoint = swedishPhonebook[midpoint].number;
 
-            }
+            List<(string name, int number)> dutchPhonebook = phonebook.Select(line => (new string(line.name.SkipWhile(c => !char.IsUpper(c)).Where(c => char.IsLetter(c) || c == ',').ToArray()), line.number)).ToList();
+            dutchPhonebook = dutchPhonebook.OrderBy(l => l.name).Select(p => (phonebook.First(pb => pb.number == p.number).name, p.number)).ToList();
+            long dutchMidpoint = dutchPhonebook[midpoint].number;
+
+            long answer = englishMidpoint * swedishMidpoint * dutchMidpoint;
 
             return answer.ToString();
         }
